f41946507f1b2270837e3c70f0b3c54b31bbdbe0
Shivam Goyal <shivamgoyal1506@outlook.com>
2019-06-08 18:36:22 +0530

    Bug Fixed: Navbar on Mobiles

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,14 @@ window.onload = function () {
     setTimeout(() => {
         var loader = document.getElementById('preloader');
         loader.style.display = 'none';
-        loader.innerHTML = ' ';
+        loader.innerHTML = ' '; // Remove content to save memory
     }, 500);
 };
 
 
 // Navbar
 var nav = document.getElementById('navbar');
+var navxs = document.getElementById('nav-row-xs');
 var isNavOpen = false;
 
 function toggleNav() {
@@ -23,18 +24,39 @@ function toggleNav() {
 	}
 }
 
+
+// Onscroll on Window
+
 window.onscroll = function(){reduceNav()};
 
 function reduceNav(){
 	if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){
-		nav.style.padding = "0px 0px";
-		nav.style.boxShadow = "0px 8px 16px 0px rgba(0,0,0,0.2)";
-		nav.style.backgroundColor = "#111111";
+		if(isMobile()){
+			navxs.style.backgroundColor = "#111111";
+			navxs.style.padding = "2px 5px 2px 15px";
+		}
+		else {
+			nav.style.padding = "0px 0px";
+			nav.style.boxShadow = "0px 8px 16px 0px rgba(0,0,0,0.2)";
+			nav.style.backgroundColor = "#111111";
+		}
 	}
 	else {
-		nav.style.padding = "20px 60px";
-		nav.style.boxShadow = "0px 8px 16px 0px rgba(0,0,0,0.0)";
-		nav.style.background = "none";
+		if(isMobile()){
+			navxs.style.background = "none";
+			navxs.style.padding = "2px 5px 2px 15px";
+		}
+		else {
+			nav.style.padding = "20px 60px";
+			nav.style.boxShadow = "0px 8px 16px 0px rgba(0,0,0,0.0)";
+			nav.style.background = "none";
+		}
 	}
 }
 
+function isMobile() {
+	if(navxs.style.display == "none" || !navxs.style.display){
+		return false;
+	}
+	else return true;
+}
